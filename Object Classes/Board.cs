f40906ebd7543e109c367d6e687bce4a9a36fbf3
Mitch Fox <39552084+mitchfox@users.noreply.github.com>
2019-06-06 10:39:12 +1000

    final commit

---
+++ Object Classes/Board.cs
@@ -0,0 +1,172 @@
+﻿using System.Diagnostics;
+
+namespace Object_Classes
+{
+    /// <summary>
+    /// Models a game board for Space Race consisting of three different types of squares
+    /// 
+    /// Ordinary squares, Wormhole squares and Blackhole squares.
+    /// 
+    /// landing on a Wormhole or Blackhole square at the end of a player's move 
+    /// results in the player moving to another square
+    /// 
+    /// </summary>
+    public static class Board
+    {
+        /// <summary>
+        /// Models a game board for Space Race consisting of three different types of squares
+        /// 
+        /// Ordinary squares, Wormhole squares and Blackhole squares.
+        /// 
+        /// landing on a Wormhole or Blackhole square at the end of a player's move 
+        /// results in the player moving to another square
+        /// 
+        /// 
+        /// </summary>
+
+        public const int NUMBER_OF_SQUARES = 56;
+        public const int START_SQUARE_NUMBER = 0;
+        public const int FINISH_SQUARE_NUMBER = NUMBER_OF_SQUARES - 1;
+
+        private static Square[] squares = new Square[NUMBER_OF_SQUARES];
+
+        public static Square[] Squares
+        {
+            get
+            {
+                Debug.Assert(squares != null, "squares != null",
+                   "The game board has not been instantiated");
+                return squares;
+            }
+        }
+
+        public static Square StartSquare
+        {
+            get
+            {
+                return squares[START_SQUARE_NUMBER];
+            }
+        }
+
+
+        /// <summary>
+        ///  Eight Wormhole squares.
+        ///  
+        /// Each row represents a Wormhole square number, the square to jump forward to and the amount of fuel consumed in that jump.
+        /// 
+        /// For example {2, 22, 10} is a Wormhole on square 2, jumping to square 22 and using 10 units of fuel
+        /// 
+        /// </summary>
+        private static int[,] wormHoles =
+        {
+            {2, 22, 10},
+            {3, 9, 3},
+            {5, 17, 6},
+            {12, 24, 6},
+            {16, 47, 15},
+            {29, 38, 4},
+            {40, 51, 5},
+            {45, 54, 4}
+        };
+
+        /// <summary>
+        ///  Eight Blackhole squares.
+        ///  
+        /// Each row represents a Blackhole square number, the square to jump back to and the amount of fuel consumed in that jump.
+        /// 
+        /// For example {10, 4, 6} is a Blackhole on square 10, jumping to square 4 and using 6 units of fuel
+        /// 
+        /// </summary>
+        private static int[,] blackHoles =
+        {
+            {10, 4, 6},
+            {26, 8, 18},
+            {30, 19, 11},
+            {35,11, 24},
+            {36, 34, 2},
+            {49, 13, 36},
+            {52, 41, 11},
+            {53, 42, 11}
+        };
+
+
+        /// <summary>
+        /// Parameterless Constructor
+        /// Initialises a board consisting of a mix of Ordinary Squares,
+        ///     Wormhole Squares and Blackhole Squares.
+        /// 
+        /// Pre:  none
+        /// Post: board is constructed
+        /// </summary>
+        public static void SetUpBoard()
+        {
+
+            // Create the 'start' square where all players will start.
+            squares[START_SQUARE_NUMBER] = new Square("Start", START_SQUARE_NUMBER);
+
+            // Create the main part of the board, squares 1 .. 54
+
+            for (int i = 1; i < NUMBER_OF_SQUARES - 1; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (i == blackHoles[j, 0])
+                    {
+                        squares[i] = new BlackholeSquare("Square " + i, blackHoles[j, 0], blackHoles[j, 1], blackHoles[j, 2]);
+                    }
+                    else if (i == wormHoles[j, 0])
+                    {
+                        squares[i] = new WormholeSquare("Square " + i, wormHoles[j, 0], wormHoles[j, 1], wormHoles[j, 2]);
+                    }
+                    else
+                    {
+                        squares[i] = new Square("Square " + i, i);
+                    }
+                }
+            }
+            //   Need to call the appropriate constructor for each square
+            //       either new Square(...),  new WormholeSquare(...) or new BlackholeSquare(...)
+            //
+
+            // Create the 'finish' square.
+            squares[FINISH_SQUARE_NUMBER] = new Square("Finish", FINISH_SQUARE_NUMBER);
+        } // end SetUpBoard
+
+        /// <summary>
+        /// Finds the destination square and the amount of fuel used for either a 
+        /// Wormhole or Blackhole Square.
+        /// 
+        /// pre: squareNum is either a Wormhole or Blackhole square number
+        /// post: destNum and amount are assigned correct values.
+        /// </summary>
+        /// <param name="holes">a 2D array representing either the Wormholes or Blackholes squares information</param>
+        /// <param name="squareNum"> a square number of either a Wormhole or Blackhole square</param>
+        /// <param name="destNum"> destination square's number</param>
+        /// <param name="amount"> amont of fuel used to jump to the deestination square</param>
+        private static void FindDestSquare(int[,] holes, int squareNum, out int destNum, out int amount)
+        {
+            const int /*start = 0,*/ exit = 1, fuel = 2;
+            destNum = 0; amount = 0;
+
+            for (int j = 0; j < 7; j++)
+            {
+                if (squareNum == blackHoles[j, 0])
+                {
+                    destNum = blackHoles[j, 1];
+                    amount = blackHoles[j, 2];
+                }
+                else if (squareNum == wormHoles[j, 0])
+                {
+                    destNum = wormHoles[j, 1];
+                    amount = wormHoles[j, 2];
+                }
+                else
+                {
+                    destNum = destNum + exit;
+                    amount = fuel;
+                }
+            }
+        } //end FindDestSquare
+
+    } //end class Board
+}
---
+++ Space Race/ConsoleInterface.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using Game_Logic_Class;
+using Object_Classes;
+
+
+namespace Space_Race
+{
+    class Console_Class
+    {
+        /// <summary>
+        /// Algorithm below currently plays only one game
+        /// 
+        /// when have this working correctly, add the abilty for the user to 
+        /// play more than 1 game if they choose to do so.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+            /*                    
+             Set up the board in Board class (Board.SetUpBoard)
+             Determine number of players - initally play with 2 for testing purposes 
+             Create the required players in Game Logic class
+              and initialize players for start of a game             
+             loop  until game is finished           
+                call PlayGame in Game Logic class to play one round
+                Output each player's details at end of round
+             end loop
+             Determine if anyone has won
+             Output each player's details at end of the game
+           */
+
+
+
+
+            // Initialise Board
+            Board.SetUpBoard();
+
+
+            do
+            {
+                // Ask for Number of Players
+                int totalPlayers = InputPlayerTotal();
+               
+                // Set Number of Players to Total Game Players
+                SpaceRaceGame.NumberOfPlayers = totalPlayers;
+
+                // Assign Players a Name from Player Name Array
+                SpaceRaceGame.SetUpPlayers();
+
+                // Set Game Bool to False
+                SpaceRaceGame.GameFinished = false;
+
+                // Initialise and Set Initial Round Value
+                int RoundNumber = 0;
+
+                // Loop Game until Game Finished is equal to True
+                while (!SpaceRaceGame.GameFinished)
+                {
+
+                    // Count Round Numbers
+                    RoundStart(RoundNumber);
+                    // Play Round
+                    SpaceRaceGame.PlayOneRound();
+                    ShowRoundResults();
+
+                    // round++;
+                    RoundNumber++;
+
+
+                }
+
+                // Show End Game Results
+                PrintGameResults();
+
+
+            } while (NewGame());
+
+            // Display Thankyou Message & Press Enter to Finish
+            ThankyouMsg();
+            PressEnter();
+
+
+
+        }//end Main
+
+
+        /// <summary>
+        /// Display a welcome message to the console
+        /// Pre:    none.
+        /// Post:   A welcome message is displayed to the console.
+        /// </summary>
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("\tWelcome to Space Race.\n");
+        } //end DisplayIntroductionMessage
+
+        /// <summary>
+        /// Displays a prompt and waits for a keypress.
+        /// Pre:  none
+        /// Post: a key has been pressed.
+        /// </summary>
+        public static void PressEnter()
+        {
+            Console.WriteLine("\tPress Enter to terminate program ...");
+            Console.ReadLine();
+        } // end PressAny
+
+        /// <summary>
+        /// Repeatedly ask for a valid number of players and return the input 
+        /// </summary>
+        static int InputPlayerTotal()
+        {
+            // Collect User Input
+            //string UserInput = "";
+            // Set Initial total to 0
+            int totalPlayers = 0;
+
+            // Prompt User for Total Game Players
+            Console.WriteLine("\tThis game is for 2 to 6 players.");
+            Console.Write("\tEnter Total Number of players to Play Space Race: ");
+
+            // Read User Input for Total Players
+            string UserInput = Console.ReadLine();
+
+
+            // Test Input & Loop until Acceptable Number was Entered
+            if ((int.TryParse(UserInput, out totalPlayers)) && (totalPlayers >= SpaceRaceGame.MAX_PLAYERS) && (totalPlayers <= SpaceRaceGame.MAX_PLAYERS))
+            {
+                return totalPlayers;
+            }
+            else
+            {
+                Console.WriteLine("Invalid number of players entered. Please Try Again");
+                return InputPlayerTotal();
+            }
+        }
+
+        /// <summary>
+        /// Display the name, position and fuel of every player at that round
+        /// </summary>
+        static void ShowRoundResults()
+        {
+            foreach (Player player in SpaceRaceGame.Players)
+            {
+                Console.WriteLine(String.Format("\t{0} on square {1} with {2} yottawatt of power remaining", player.Name, player.Position, player.RocketFuel));
+            }
+        }
+
+        static void RoundStart(int round_number)
+        {
+
+            Console.WriteLine("\n\nPress Enter to play a round...");
+            Console.ReadLine();
+            if (round_number == 0)
+            {
+                Console.WriteLine("\n\tFirst Round\n");
+            }
+            else
+            {
+                Console.WriteLine("\n\tNext Round\n");
+            }
+        }
+
+        /// <summary>
+        /// Ask the user to enter Y or N
+        /// Returns true if the user wishes to continue
+        /// False if they want to exit
+        /// Or asks again if they enter an invalid answer
+        /// </summary>
+        static bool NewGame()
+        {
+            string newGameInput = "";
+
+            Console.Write("\nWould You Like To Play Again? (Y or N): ");
+            newGameInput = Console.ReadLine();
+
+            // If User enters 'Y', a New Game is created
+            if (newGameInput == "Y")
+            {
+                return true;
+            }
+
+            // Likewise Game is Finished when User enters 'N'
+            else if (newGameInput == "N")
+            {
+                return false;
+            }
+
+            // If Input is Not Valid, Repeat Question
+            else
+            {
+                Console.Write("\nInvalid Input");
+                return NewGame();
+            }
+        }
+
+        /// <summary>
+        /// Display the game results and
+        /// wait for user to press enter key to advance
+        /// </summary>
+        static void PrintGameResults()
+        {
+            // Display Game Results
+            Console.WriteLine(SpaceRaceGame.DisplayGameResults());
+            //Individual Results
+            Console.WriteLine("\n\nIndividual players finished at the locations specified.");
+            foreach (Player player in SpaceRaceGame.Players)
+            {
+                Console.WriteLine(String.Format("\n\n\n{0} with {1} yottawatt of power at square {2}", player.Name, player.RocketFuel, player.Position));
+            }
+
+            // Wait for Enter Press
+            Console.WriteLine("\n\n\nPress Enter key to continue...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Display the exit message.
+        /// </summary>
+        static void ThankyouMsg()
+        {
+
+            Console.WriteLine("\n\n\nThank You For Playing Space Race! See You Next Time");
+
+        }
+    } //end Console class
+}
